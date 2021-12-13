725f6b0240331d4ac6c1475a1a005513a0e49678
AlvarTR <alvar.tapiaruiz@gmail.com>
2021-12-13 17:28:20 +0100

    Non zero consecutives skeleton

--- Code/#305 Remove consecutive sum 0 nodes.py
+++ Code/#305 Remove consecutive sum 0 nodes.py
@@ -5,5 +5,28 @@ For example, suppose you are given the input 3 -> 4 -> -7 -> 5 -> -6 -> 6. In th
 """
 from chrono import chrono
 
+def nonZeroConsecutives(lista):
+    totals = []
+    for num in lista:
+        totals.append(0)
+        for i, t in enumerate(totals):
+            currentValue = num + t
+            if currentValue == 0:
+                break
+            totals[i] = currentValue
+        else:
+            print("i =", i)
+            while len(totals) > i:
+                totals.pop()
+    return totals
+
+
+
 if __name__ == "__main__":
-    pass
+    lists = [
+    [1, 2, 3],
+    [1, 2, 3, -6],
+    [3, 4, -7, 5, -6, 6],
+    ]
+    for l in lists:
+        print(l, nonZeroConsecutives(l))
