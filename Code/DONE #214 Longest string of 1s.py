23cdff5b6b01a0b55295d79c5e63cf5ca803ce0f
AlvarTR <67113241+AlvarTR@users.noreply.github.com>
2021-12-20 14:17:33 +0100

    Update and rename #214 Longest string of 1s.py to DONE #214 Longest string of 1s.py

--- Code/DONE #214 Longest string of 1s.py
+++ Code/DONE #214 Longest string of 1s.py
@@ -3,25 +3,25 @@ Given an integer n, return the length of the longest consecutive run of 1s in it
 
 For example, given 156, you should return 3.
 """
+from sys import getsizeof
 
-#Doesn't work for negatives
 def longestSequenceOfOnes(number):
-    maxSequence = 0
-    currentSequence = 0
-    mask = 1
-    numberCopy = number
-    while numberCopy != 0:
-        if numberCopy & mask:
+    numberSize = getsizeof(number)
+    maxSequence, currentSequence = 0, 0
+    mask, maskSize = 1, 1
+    while maskSize <= numberSize:
+        if number & mask:
             currentSequence += 1
         else:
             if maxSequence < currentSequence:
                 maxSequence = currentSequence
             currentSequence = 0
-        numberCopy >>= 1
+        mask <<= 1
+        maskSize += 1
 
     return max(maxSequence, currentSequence)
 
 if __name__ == "__main__":
-    numbers = [156, 31, 8]
+    numbers = [156, 31, 32, -3, -2, -1, 65535]
     for num in numbers:
         print(num, longestSequenceOfOnes(num))
